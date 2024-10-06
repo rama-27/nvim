e573be46ea071041279e0c167c507778bfef4d9d
rama-27 <ramarajumantena231@gmail.com>
2024-10-06 15:18:47 +0530

    yo

---
+++ lua/theprimeagen/lazy/init.lua
@@ -0,0 +1,28 @@
+return {
+
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    "eandrju/cellular-automaton.nvim",
+    {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+},
+}
+
